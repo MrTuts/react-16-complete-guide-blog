cd7fcf82c0c54ac25ad0ef556cdbc75102e37b23
Pavel Gric <pavel@ideatech.cz>
2019-01-29 13:34:56 +0100

    [WIP] Router linking - Redirect programatical

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -19,14 +19,15 @@ class NewPost extends Component {
       author: this.state.author,
     };
     axios.post('/posts', data).then(response => {
-      this.setState({ submitted: true });
+      this.props.history.replace('/posts');
+      // this.setState({ submitted: true });
     });
   };
 
   render() {
     return (
       <div className="NewPost">
-        {this.state.submitted && <Redirect to="posts/" />}
+        {/*this.state.submitted && <Redirect to="posts/" />*/}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
